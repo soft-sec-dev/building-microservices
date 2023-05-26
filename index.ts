b72d08ad69ae8be3c3507d6feba6d05cd53f0e6d
ubuntu-jordan <jordanelcracken@gmail.com>
2023-05-26 03:22:54 -0500

    feat: set up

---
+++ index.ts
@@ -0,0 +1,4 @@
+import RunApp from "./src/app";
+
+const app = new RunApp()
+app.listen()
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
 import express from 'express'
+import { Express, Request, Response, RequestHandler } from 'express'
+
+export default class RunApp {
+    private app: Express = express()
+    private PORT: undefined | number | string = process.env.PORT || 3000
+
+    constructor() {
+        this.initializeMiddlewares()
+        this.initializeRoutes()
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(express.json())
+    }
+
+    private initializeRoutes() {
+        this.app.get('/', (req, res) => {
+            res.json({ status: 'succes' })
+        })
+    }
+
+    public listen() {
+        this.app.listen(this.PORT, () => {
+            console.log(`Running on: http://localhost:${this.PORT}/`)
+        })
+    }
+}
