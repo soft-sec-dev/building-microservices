1cf21e6e56725c7201638d814259a8715b3a7da8
ubuntu-jordan <jordanelcracken@gmail.com>
2023-05-26 16:49:19 -0500

    feat: publishing our first microservices

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Process
+> `docker login < ... > --username < ... > --password < ... >`
+> `docker tag video-streaming bmdksergio1.azurecr.io/video-streaming:latest`
+> `docker push bmdksergio1.azurecr.io/video-streaming`
+> `docker run -dp 3000:3000 bmdksergio1.azurecr.io/video-streaming`
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { Express, Request, Response, RequestHandler } from 'express'
+import fs from 'node:fs'
+import morgan from 'morgan'
+import path from 'node:path'
 
 export default class RunApp {
     private app: Express = express()
     private PORT: undefined | number | string = process.env.PORT || 3000
+    private videoPath = path.join(__dirname, "../videos/SampleVideo_1280x720_1mb.mp4")
 
     constructor() {
         this.initializeMiddlewares()
@@ -11,6 +15,7 @@ export default class RunApp {
     }
 
     private initializeMiddlewares() {
+        this.app.use(morgan('dev'))
         this.app.use(express.json())
     }
 
@@ -18,6 +23,20 @@ export default class RunApp {
         this.app.get('/', (req, res) => {
             res.json({ status: 'succes' })
         })
+        this.app.get('/videos', (req, res) => {
+            fs.stat(this.videoPath, (err, stats) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send('Error on load video')
+                    return
+                }
+                res.writeHead(200, {
+                    "Content-Length": stats.size,
+                    "Content-Type": "video/mp4",
+                })
+                fs.createReadStream(this.videoPath).pipe(res)
+            })
+        })  
     }
 
     public listen() {
