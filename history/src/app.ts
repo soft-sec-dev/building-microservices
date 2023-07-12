8402af89090f6cb5401386a557d4af5fd94a5f68
jordanhuaman <jordanelcracken@gmail.com>
2023-07-12 16:41:50 -0500

    Fix: return to a chap-3

--- history/src/app.ts
+++ history/src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express'
 import morgan from 'morgan'
-
+import { Db } from 'mongodb'
 export class HistoryApp {
     private app: Express = express()
     constructor() {
@@ -12,8 +12,22 @@ export class HistoryApp {
         this.app.use(morgan('dev'))
     }
 
-    private setupHandlers(app: Express) {
-
+    private setupHandlers(app: Express, db:Db) {
+        const videosCollectios = db.collection("videos")
+        this.app.post('viewed', (req,res)=>{
+            const videoPath = req.body.videoPath
+            videosCollectios
+                .insertOne({videoPath:videoPath})
+                    .then(()=>{
+                        console.log(`Added video ${videoPath} to history`)
+                        res.status(200).json({data:videoPath, status:'succes'})
+                    })
+                    .catch(err=>{
+                        console.log(`Error adding video ${videoPath} to history`)
+                        console.error(err && err.stack || err)
+                        res.sendStatus(500)
+                    })
+        })
     }
     private startHttpServer(): Promise<unknown> {
         return new Promise<void>((resolve, reject) => {
