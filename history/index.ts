4d7c132f833db6ca67c39267ab85647e905996d7
jordanhuaman <jordanelcracken@gmail.com>
2023-07-18 20:34:07 -0500

    Feat: finish config db-chap5

--- history/index.ts
+++ history/index.ts
@@ -2,8 +2,4 @@ import { HistoryApp } from "./src/app";
 
 const historyApp = new HistoryApp()
 
-historyApp.main().then(() => console.log('Microservice-History online indexando'))
-    .catch(err => {
-        console.log("Microservices failed to start")
-        console.log(err && err.stack || err)
-    })
+historyApp.main()
--- history/src/app.ts
+++ history/src/app.ts
@@ -1,13 +1,13 @@
 import express, { Express, Request, Response } from 'express'
 import morgan from 'morgan'
-import { Db, MongoClient, ReturnDocument } from 'mongodb'
-import mongodb from 'mongodb'
+import mongodb,{Db, MongoClient} from 'mongodb'
 export class HistoryApp {
     private app: Express = express()
+    private port = process.env.PORT || 4003
     private DBHOST = process.env.DBHOST as string
     private DBNAME = process.env.DBNAME
-    private client = new MongoClient(`${this.DBHOST}`)
-    private collections:any
+    // private client = new MongoClient(`${this.DBHOST}`)
+    // private collections:any
     constructor() {
         this.initilizeMiddelwares()
     }
@@ -19,6 +19,11 @@ export class HistoryApp {
 
     private setupHandlers(app: Express, db:Db) {
         const videosCollectios = db.collection("videos")
+
+        this.app.get('/', (req,res)=>{
+            res.status(200).json({data:'nothing for now', msg:'ok'})
+        })
+    
         this.app.post('viewed', (req,res)=>{
             const videoPath = req.body.videoPath
             videosCollectios
@@ -33,25 +38,46 @@ export class HistoryApp {
                         res.sendStatus(500)
                     })
         })
+        this.app.get('/history', (req,res)=>{
+            const skip = parseInt(req.query.skip as string)
+            const limit = parseInt(req.query.limit as string)
+            videosCollectios.find()
+                .skip(skip)
+                .limit(limit)
+                .toArray()
+                .then(documentos => {
+                    res.json({history: documentos})
+                }).catch(err =>{
+                    console.error(`Error retrieving from database`)
+                    console.error(err && err.stack || err)
+                    res.status(500)
+                })
+        }) 
+    }
+    private async conectDB(){
+        try{
+            const db = new MongoClient(this.DBHOST)
+            return db.db()
+        } catch(err){
+            console.error('Error al conectarse a la base de datps')
+            process.exit(1)
+        }
     }
 
-    private startHttpServer(db:Db): Promise<unknown> {
-        return new Promise<void>((resolve, reject) => {
-            this.setupHandlers(this.app, db)
-            const port = process.env.PORT || 4003
-            this.app.listen(port, () => {
-                resolve()
-            })
+    private startHttpServer(db:Db) {
+        this.setupHandlers(this.app, db)
+        this.app.listen(this.port, () => {
+            console.log(`History runnin on: http://localhost:${this.port}/`)
         })
     }
-
+    
     public async main() {
-        try{
-            await this.client.connect()
-            const db = this.client.db(`${this.DBNAME}`)
-            this.collections = db.collection('videos')
-        }catch(err){
-            console.log('Error al conectarse a mondodb en HISTORY', err)
-        }
+        return this.conectDB()
+        .then(db =>{
+            this.startHttpServer(db)
+        }).catch(err =>{
+            console.error("Microservice failed to start.");
+            console.error(err && err.stack || err);
+        })
     }
 }
